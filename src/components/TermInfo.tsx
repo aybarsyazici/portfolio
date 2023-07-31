fc2d3045fe7ee52fa97088d75299c4d57eed884f
aybarsyazici <aybarsyazici@sabanciuniv.edu>
2023-07-31 17:16:44 +0300

    chore: changed term info text

--- src/components/TermInfo.tsx
+++ src/components/TermInfo.tsx
@@ -3,7 +3,7 @@ import { User, WebsiteName, Wrapper } from "./styles/TerminalInfo.styled";
 const TermInfo = () => {
   return (
     <Wrapper>
-      <User>visitor</User>@<WebsiteName>terminal.aybarsyazici.dev</WebsiteName>
+      <User>visitor</User>@<WebsiteName>aybarsyazici.com</WebsiteName>
       :~$
     </Wrapper>
   );
