1eac54d99df78ac5ecab7ab4f59f3345dd50e764
Aybars Yazici <aybarsyazici@sabanciuniv.edu>
2024-10-16 18:06:12 +0200

    feat: added google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-V0PG81QQFD");
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
